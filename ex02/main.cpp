26fc1546163fd486bd9cca203eddcc4a81138c7a
Samuele Dell'Erba <sdell-er@c1r6p12.42firenze.it>
2025-01-18 18:57:05 +0100

    fine

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: samuele <samuele@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sdell-er <sdell-er@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 23:51:04 by samuele           #+#    #+#             */
-/*   Updated: 2025/01/17 22:45:56 by samuele          ###   ########.fr       */
+/*   Updated: 2025/01/18 18:56:36 by sdell-er         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,9 @@
 #include "PmergeMe.hpp"
 
 template <typename T>
-T stringToNumber(std::string str)
-{
-    T number;
-    std::stringstream ss(str);
-
-    ss >> number;
-    return number;
-}
+T stringToNumber(std::string str);
+template <typename T>
+void fillContainer(T &container, char **argv, int argc);
 
 int main(int argc, char **argv)
 {
@@ -35,16 +30,7 @@ int main(int argc, char **argv)
     std::clock_t start_v = std::clock();
 
     std::vector<int> numbers_v;
-
-    for (int i = 1; i < argc; i++)
-    {
-        if (std::string(argv[i]).find_first_not_of("0123456789") != std::string::npos)
-        {
-            std::cerr << "Error: invalid sequence" << std::endl;
-            return 1;
-        }
-        numbers_v.push_back(stringToNumber<int>(argv[i]));
-    }
+    fillContainer(numbers_v, argv, argc);
 
     mergeInsertion(numbers_v);
     
@@ -55,24 +41,14 @@ int main(int argc, char **argv)
     std::clock_t start_l = std::clock();
 
     std::list<int> numbers_l;
-
-    for (int i = 1; i < argc; i++)
-    {
-        if (std::string(argv[i]).find_first_not_of("0123456789") != std::string::npos)
-        {
-            std::cerr << "Error: invalid sequence" << std::endl;
-            return 1;
-        }
-        numbers_l.push_back(stringToNumber<int>(argv[i]));
-    }
+    fillContainer(numbers_l, argv, argc);
 
     mergeInsertion(numbers_l);
     
     double duration_l = (std::clock() - start_l) / (double) CLOCKS_PER_SEC;
 
     std::vector<int> original;
-    for (int i = 1; i < argc; i++)
-        original.push_back(stringToNumber<int>(argv[i]));
+    fillContainer(original, argv, argc);
     std::cout << "Before:" << std::endl;
     for (size_t i = 0; i < original.size(); i++)
         std::cout << original[i] << " ";
@@ -85,4 +61,28 @@ int main(int argc, char **argv)
     std::cout << std::fixed << "Time to process a range of " << numbers_l.size() << " elements with std::list:   " << duration_l << "s" << std::endl;
     
     return 0;
+}
+
+template <typename T>
+void fillContainer(T &container, char **argv, int argc)
+{
+	for (int i = 1; i < argc; i++)
+	{
+		if (std::string(argv[i]).find_first_not_of("0123456789") != std::string::npos)
+		{
+			std::cerr << "Error: invalid sequence" << std::endl;
+			exit(1);
+		}
+		container.push_back(stringToNumber<int>(argv[i]));
+	}
+}
+
+template <typename T>
+T stringToNumber(std::string str)
+{
+    T number;
+    std::stringstream ss(str);
+
+    ss >> number;
+    return number;
 }
